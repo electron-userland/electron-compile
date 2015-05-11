5c9b06d18faf7d1b2b8e5775161630b68198b182
Paul Betts <paul@paulbetts.org>
2015-05-11 00:32:45 -0700

    Add TypeScript support

---
+++ src/typescript.js
@@ -0,0 +1,40 @@
+import _ from 'lodash';
+import CompileCache from './compile-cache';
+
+let tss = null;
+
+export default class TypeScriptCompiler extends CompileCache {
+  constructor(options={}) {
+    super();
+    
+    this.compilerInformation = _.extend({}, options, {
+      extension: 'ts',
+      target: 1,
+      module: 'commonjs',
+      sourceMap: true
+    });
+  }
+    
+  getCompilerInformation() {
+    return this.compilerInformation;
+  }
+  
+  compile(sourceCode, filePath) {
+    this.ensureTs();
+    return tss.compile(sourceCode, filePath);
+  }
+  
+  shouldCompileFile() {
+    this.ensureTs();
+    return true;
+  }
+  
+  ensureTs() {
+    if (!tss) {
+      const {TypeScriptSimple} = require('typescript-simple');
+      tss = new TypeScriptSimple(this.compilerInformation, false);
+      
+      this.compilerInformation.version = require('typescript-simple/package.json').version;
+    }
+  }
+}
