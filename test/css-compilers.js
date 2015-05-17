ff8a5c2831bdb3c6ffddcb8e3ac731519430f6f4
Paul Betts <paul@paulbetts.org>
2015-05-17 16:21:57 -0700

    Tests for SCSS

--- test/css-compilers.js
+++ test/css-compilers.js
@@ -4,13 +4,14 @@ import fs from 'fs';
 import path from 'path';
 
 import LessCompiler from '../lib/css/less';
+import ScssCompiler from '../lib/css/scss';
 
 describe('The LESS Compiler', function() {
   it('should compile valid LESS', function() {
     let fixture = new LessCompiler();
     
     let input = path.join(__dirname, '..', 'test', 'fixtures', 'valid.less');
-    let result = fixture.compile(fs.readFileSync(input, 'utf8'));
+    let result = fixture.compile(fs.readFileSync(input, 'utf8'), input);
     
     expect(result.length > 0).to.be.ok;
   });
@@ -22,7 +23,34 @@ describe('The LESS Compiler', function() {
     
     let shouldDie = true;
     try {
-      let result = fixture.compile(fs.readFileSync(input, 'utf8'));
+      let result = fixture.compile(fs.readFileSync(input, 'utf8'), input);
+      console.log(result);
+    } catch (e) {
+      shouldDie = false;
+    }
+    
+    expect(shouldDie).not.to.be.ok;
+  });
+});
+
+describe('The Sass Compiler', function() {
+  it('should compile valid Sass', function() {
+    let fixture = new ScssCompiler();
+    
+    let input = path.join(__dirname, '..', 'test', 'fixtures', 'valid.scss');
+    let result = fixture.compile(fs.readFileSync(input, 'utf8'), input);
+    
+    expect(result.length > 0).to.be.ok;
+  });
+  
+  it('should fail on invalid Sass', function() {
+    let fixture = new ScssCompiler();
+    
+    let input = path.join(__dirname, '..', 'test', 'fixtures', 'invalid.sass');
+    
+    let shouldDie = true;
+    try {
+      let result = fixture.compile(fs.readFileSync(input, 'utf8'), input);
       console.log(result);
     } catch (e) {
       shouldDie = false;
