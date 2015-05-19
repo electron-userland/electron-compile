44880b07ac03c48bcdddaf49f4213fe49c6d7b7d
Paul Betts <paul@paulbetts.org>
2015-05-18 20:41:47 -0700

    Simplify the compiler initialization

--- src/compile-cache.js
+++ src/compile-cache.js
@@ -12,35 +12,39 @@ export default class CompileCache {
       hits: 0,
       misses: 0
     };
-    
+
     this.cacheDir = null;
     this.jsCacheDir = null;
     this.seenFilePaths = {};
   }
-    
+
   getCompilerInformation() {
     throw new Error("Implement this in a derived class");
   }
-  
+
   compile(sourceCode, filePath, cachePath) {
     throw new Error("Implement this in a derived class");
   }
-  
+
   getMimeType() {
     throw new Error("Implement this in a derived class");
   }
-  
+
+  initializeCompiler() {
+    throw new Error("Implement this in a derived class");
+  }
+
   shouldCompileFile(sourceCode, fullPath) {
     this.ensureInitialized();
     let lowerPath = fullPath.toLowerCase();
-    
-    // NB: require() normally does this for us, but in our protocol hook we 
+
+    // NB: require() normally does this for us, but in our protocol hook we
     // need to do this ourselves
     return _.some(
-      this.extensions, 
+      this.extensions,
       (ext) => lowerPath.lastIndexOf(ext) + ext.length === lowerPath.length);
   }
-  
+
   ///
   /// shasum - Hash with an update() method.
   /// value - Must be a value that could be returned by JSON.parse().
@@ -51,24 +55,24 @@ export default class CompileCache {
     // * No effort is made to avoid trailing commas.
     // These shortcuts should not affect the correctness of this function.
     const type = typeof(value);
-    
+
     if (type === 'string') {
       shasum.update('"', 'utf8');
       shasum.update(value, 'utf8');
       shasum.update('"', 'utf8');
       return;
     }
-    
+
     if (type === 'boolean' || type === 'number') {
       shasum.update(value.toString(), 'utf8');
       return;
     }
-    
+
     if (value === null) {
       shasum.update('null', 'utf8');
       return;
     }
-    
+
     if (Array.isArray(value)) {
       shasum.update('[', 'utf8');
       for (let i=0; i < value.length; i++) {
@@ -78,20 +82,20 @@ export default class CompileCache {
       shasum.update(']', 'utf8');
       return;
     }
-    
+
     // value must be an object: be sure to sort the keys.
     let keys = Object.keys(value);
     keys.sort();
 
     shasum.update('{', 'utf8');
-    
+
     for (let i=0; i < keys.length; i++) {
       this.updateDigestForJsonValue(shasum, keys[i]);
       shasum.update(': ', 'utf8');
       this.updateDigestForJsonValue(shasum, value[keys[i]]);
       shasum.update(',', 'utf8');
     }
-    
+
     shasum.update('}', 'utf8');
   }
 
@@ -100,7 +104,7 @@ export default class CompileCache {
     this.updateDigestForJsonValue(sha1, this.getCompilerInformation());
     return sha1.digest('hex');
   }
-  
+
   getCachePath(sourceCode) {
     let digest = crypto.createHash('sha1').update(sourceCode, 'utf8').digest('hex');
 
@@ -116,46 +120,46 @@ export default class CompileCache {
     try {
       let ret = fs.readFileSync(cachePath, 'utf8');
       this.stats.hits++;
-      
+
       return ret;
     } catch (e) {
       return null;
     }
   }
-  
+
   // Function that obeys the contract of an entry in the require.extensions map.
   // Returns the transpiled version of the JavaScript code at filePath, which is
   // either generated on the fly or pulled from cache.
   loadFile(module, filePath, returnOnly=false, sourceCode=null) {
     this.ensureInitialized();
-    
+
     let fullPath = path.resolve(filePath);
     this.seenFilePaths[path.dirname(filePath)] = true;
-    
+
     sourceCode = sourceCode || fs.readFileSync(filePath, 'utf8');
-    
+
     if (!this.shouldCompileFile(sourceCode, fullPath)) {
       if (returnOnly) return sourceCode;
       return module._compile(sourceCode, filePath);
     }
-    
+
     let cachePath = this.getCachePath(sourceCode);
     let js = this.getCachedJavaScript(cachePath);
-    
+
     if (!js) {
       js = this.compile(sourceCode, filePath, cachePath);
       this.stats.misses++;
-      
+
       fs.writeFileSync(cachePath, js);
-    } 
-    
+    }
+
     if (returnOnly) return js;
     return module._compile(js, filePath);
   }
-  
+
   register() {
     this.ensureInitialized();
-    
+
     for (let i=0; i < this.extensions.length; i++) {
       Object.defineProperty(require.extensions, `.${this.extensions[i]}`, {
         enumerable: true,
@@ -164,22 +168,22 @@ export default class CompileCache {
       });
     }
   }
-  
+
   ensureInitialized() {
     if (this.extensions) return;
-    
+
     let info = this.getCompilerInformation();
-    
+
     if (!info.extension && !info.extensions) {
       throw new Error("Compiler must register at least one extension in getCompilerInformation");
     }
-    
+
     this.extensions = (info.extensions ? info.extensions : [info.extension]);
   }
-  
+
   setCacheDirectory(newCacheDir) {
     if (this.cacheDir === newCacheDir) return;
-    
+
     this.cacheDir = newCacheDir;
     this.jsCacheDir = null;
   }
--- src/css/scss.js
+++ src/css/scss.js
@@ -11,30 +11,28 @@ const scssFileExtensions = /\.(sass|scss)$/i;
 export default class ScssCompiler extends CompileCache {
   constructor(options={}) {
     super();
-    
+
     const defaultOptions = {
       sourceComments: true,
       sourceMapEmbed: true,
       sourceMapContents: true,
     };
-    
-    const requiredOptions = { 
+
+    const requiredOptions = {
       extensions: ['scss', 'sass']
     };
-    
+
     this.compilerInformation = _.extend(defaultOptions, options, requiredOptions);
   }
-    
+
   getCompilerInformation() {
     return this.compilerInformation;
   }
-  
+
   compile(sourceCode, filePath) {
-    this.ensureScss();
-    
     let paths = Object.keys(this.seenFilePaths);
     paths.unshift('.');
-    
+
     let opts = _.extend({}, this.compilerInformation, {
       data: sourceCode,
       indentedSyntax: filePath.match(/\.sass$/i),
@@ -42,27 +40,17 @@ export default class ScssCompiler extends CompileCache {
       includePaths: paths,
       filename: path.basename(filePath)
     });
-    
+
     let result = scss.renderSync(opts);
     return result.toString('utf8');
   }
-   
-  shouldCompileFile(sourceCode, fullPath) {
-    let ret = super.shouldCompileFile(sourceCode, fullPath);
-    if (!ret) return ret;
-    
-    this.ensureScss();
-    return ret;
-  }
-  
+
   getMimeType() { return 'text/css'; }
-  
+
   register() {}
-  
-  ensureScss() {
-    if (!scss) {
-      scss = require('node-sass');
-      this.compilerInformation.version = require('node-sass/package.json').version;
-    }
+
+  initializeCompiler() {
+    scss = require('node-sass');
+    return require('node-sass/package.json').version;
   }
 }
