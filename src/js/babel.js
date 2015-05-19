926fa6694c45585a3b11bbf3ca26f59b71af425a
Paul Betts <paul@paulbetts.org>
2015-05-18 23:14:16 -0700

    No directive in our own code

--- src/js/babel.js
+++ src/js/babel.js
@@ -1,5 +1,3 @@
-'use babel';
-
 import _ from 'lodash';
 import CompileCache from '../compile-cache';
 
--- src/js/coffeescript.js
+++ src/js/coffeescript.js
@@ -1,5 +1,3 @@
-'use babel';
-
 import _ from 'lodash';
 import path from 'path';
 import btoa from 'btoa';
