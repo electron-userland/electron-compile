0b62fa83e5d4fc54522c6e748f8f38491629c5b5
Paul Betts <paul@paulbetts.org>
2015-06-27 18:58:26 -0500

    Create compilers for testing before each test

--- test/main.js
+++ test/main.js
@@ -1,24 +1,37 @@
 require('./support.js');
 
+import _ from 'lodash';
 import {compile} from '../lib/main';
 import path from 'path';
+import TypeScriptCompiler from '../lib/js/typescript';
+import LessCompiler from '../lib/css/less';
 
 describe('exports for this library', function() {
   describe('the compile method', function() {
+    beforeEach(function() {
+      this.compilers = [
+        new TypeScriptCompiler(),
+        new LessCompiler()
+      ];
+      
+      // Disable caching on compilers
+      _.each(this.compilers, (x) => x.setCacheDirectory(null));
+    });
+    
     it('should compile a LESS file', function() {
-      let result = compile(path.resolve(__dirname, 'fixtures', 'valid.less'));
+      let result = compile(path.resolve(__dirname, '..', 'test', 'fixtures', 'valid.less'), this.compilers);
       expect(result.length > 10).to.be.ok;
     });
     
     it('should passthrough files it doesnt recognize', function() {
-      let result = compile(path.resolve(__dirname, '..', '.gitignore'));
+      let result = compile(path.resolve(__dirname, '..', '.gitignore'), this.compilers);
       expect(result.length > 10).to.be.ok;
     });
     
     it('should blow up on bad input', function() {
       let shouldDie = true;
       try {
-        compile(path.resolve(__dirname, 'fixtures', 'invalid.ts'));
+        compile(path.resolve(__dirname, '..', 'test', 'fixtures', 'invalid.ts'), this.compilers);
       } catch (e) {
         shouldDie = false;
       }
