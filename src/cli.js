be5e7d6b91eff558980f5552480272f2d57d3ab2
Paul Betts <paul@paulbetts.org>
2015-06-27 20:13:03 -0500

    Allow multiple paths for source

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,10 @@
 
 import {compile, init} from './main';
 import forAllFiles from './for-all-files';
+import _ from 'lodash';
 
 const yargs = require('yargs')
-  .usage('Usage: electron-compile --target [target-path] [source-path]')
+  .usage('Usage: electron-compile --target [target-path] paths...')
   .alias('t', 'target')
   .describe('t', 'The target directory to write a cache directory to')
   .alias('v', 'verbose')
@@ -15,28 +16,31 @@ const yargs = require('yargs')
   
 const argv = yargs.argv;
 
-if (!argv._ || argv._.length !== 1) {
+if (!argv._ || argv._.length < 1) {
   yargs.showHelp();
   process.exit(-1);
 }
 
-const sourceDir = argv._[0];
+const sourceDirs = argv._;
 const targetDir = argv.t || './cache';
 
 let allSucceeded = true;
 init(targetDir, true);
-forAllFiles(sourceDir, (f) => {
-  if (argv.v) console.log(`Compiling ${f}...`);
-  try {
-    compile(f);
-  } catch (e) {
-    console.error(`Failed to compile ${f}!`);
-    console.error(e.message);
-    
-    if (argv.v) console.error(e.stack);
-    console.error("\n");
-    allSucceeded = false;
-  }
+
+_.each(sourceDirs, (sourceDir) => {
+  forAllFiles(sourceDir, (f) => {
+    if (argv.v) console.log(`Compiling ${f}...`);
+    try {
+      compile(f);
+    } catch (e) {
+      console.error(`Failed to compile ${f}!`);
+      console.error(e.message);
+      
+      if (argv.v) console.error(e.stack);
+      console.error("\n");
+      allSucceeded = false;
+    }
+  });
 });
 
 process.exit(allSucceeded ? 0 : -1);
