314f2c601ec4e6590362c286a9a75bda113ad2b1
Paul Betts <paul@paulbetts.org>
2015-06-30 17:06:16 -0700

    Infinite :memo:

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import path from 'path';
 import {initializeProtocolHook} from './protocol-hook';
 import forAllFiles from './for-all-files';
 
+// Public: Allows you to create new instances of all compilers that are 
+// supported by electron-compile and use them directly. Currently supports
+// Babel, CoffeeScript, TypeScript, LESS, and Sass/SCSS.
+//
+// Returns an {Array} of {CompileCache} objects.
 export function createAllCompilers() {
   return _.map([
     './js/babel',
@@ -21,10 +26,19 @@ export function createAllCompilers() {
 let availableCompilers = null;
 let lastCacheDir = null;
 
+// Public: Compiles a single file given its path.
+//
+// filePath: The path on disk to the file
+// compilers: (optional) - An {Array} of objects conforming to {CompileCache}
+//                         that will be tried in-order to compile code. You must
+//                         call init() first if this parameter is null.
+//
+// Returns a {String} with the compiled output, or will throw an {Error} 
+// representing the compiler errors encountered.
 export function compile(filePath, compilers=null) {
   compilers = compilers || availableCompilers;
   if (!compilers) {
-    throw new Error("Call init first or pass in an array for compilers");
+    throw new Error("Call init() first or pass in an Array to the compilers parameter");
   }
   
   let compiler = null;
@@ -35,10 +49,32 @@ export function compile(filePath, compilers=null) {
   return compiler.loadFile(null, filePath, true, sourceCode);
 }
 
+// Public: Recursively compiles an entire directory of files.
+//
+// rootDirectory: The path on disk to the directory of files to compile.
+// compilers: (optional) - An {Array} of objects conforming to {CompileCache}
+//                         that will be tried in-order to compile code.
+//
+// Returns nothing.
 export function compileAll(rootDirectory, compilers=null) {
   forAllFiles(rootDirectory, (f) => compile(f, compilers));
 }
 
+// Public: Initializes the electron-compile library. Once this method is called,
+//         all JavaScript and CSS that is loaded will now be first transpiled, in
+//         both the browser and renderer processes. 
+//
+//         Note that because of limitations in Electron, this does **not** apply 
+//         to WebView or Browser preload scripts - call init again at the top of
+//         these scripts to set everything up again.
+//
+// cacheDir: The directory to cache compiled JS and CSS to. If not given, one 
+//           will be generated from the Temp directory.
+//
+// skipRegister: Do not register with the node.js module system - this is used 
+// mostly for unit test purposes.
+//
+// Returns nothing.
 export function init(cacheDir=null, skipRegister=false) {
   if (lastCacheDir === cacheDir && availableCompilers) return;
   
