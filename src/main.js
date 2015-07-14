3f0af2dfb9f9c0dd2c46aeb092f0984ee7aa1080
Paul Betts <paul@paulbetts.org>
2015-07-13 23:53:07 -0700

    Create a method to dump compiler parameters out

--- src/main.js
+++ src/main.js
@@ -9,23 +9,23 @@ import forAllFiles from './for-all-files';
 // cache-only versions of these compilers
 let allCompilerClasses = null;
 
-// Public: Allows you to create new instances of all compilers that are 
+// Public: Allows you to create new instances of all compilers that are
 // supported by electron-compile and use them directly. Currently supports
 // Babel, CoffeeScript, TypeScript, LESS, and Sass/SCSS.
 //
 // Returns an {Array} of {CompileCache} objects.
 export function createAllCompilers(compilerOpts=null) {
   allCompilerClasses = allCompilerClasses || require('electron-compilers');
-  
+
   return _.map(allCompilerClasses, (Klass) => {
     if (!compilerOpts) return new Klass();
-    
+
     let exts = Klass.getExtensions();
     let optsForUs = _.reduce(
-      exts, 
+      exts,
       (acc,x) => _.extend(acc, compilerOpts[x] || {}),
       {});
-      
+
     return new Klass(optsForUs);
   });
 }
@@ -40,14 +40,14 @@ let lastCacheDir = null;
 //                         that will be tried in-order to compile code. You must
 //                         call init() first if this parameter is null.
 //
-// Returns a {String} with the compiled output, or will throw an {Error} 
+// Returns a {String} with the compiled output, or will throw an {Error}
 // representing the compiler errors encountered.
 export function compile(filePath, compilers=null) {
   compilers = compilers || availableCompilers;
   if (!compilers) {
     throw new Error("Call init() first or pass in an Array to the compilers parameter");
   }
-  
+
   let compiler = null;
   compiler = _.find(compilers, (x) => x.shouldCompileFile(filePath));
   if (!compiler) return fs.readFileSync(filePath, 'utf8');
@@ -70,16 +70,16 @@ export function compileAll(rootDirectory, compilers=null) {
 
 // Public: Initializes the electron-compile library. Once this method is called,
 //         all JavaScript and CSS that is loaded will now be first transpiled, in
-//         both the browser and renderer processes. 
+//         both the browser and renderer processes.
 //
-//         Note that because of limitations in Electron, this does **not** apply 
+//         Note that because of limitations in Electron, this does **not** apply
 //         to WebView or Browser preload scripts - call init again at the top of
 //         these scripts to set everything up again.
 //
-// cacheDir: The directory to cache compiled JS and CSS to. If not given, one 
+// cacheDir: The directory to cache compiled JS and CSS to. If not given, one
 //           will be generated from the Temp directory.
 //
-// skipRegister: Do not register with the node.js module system - this is used 
+// skipRegister: Do not register with the node.js module system - this is used
 // mostly for unit test purposes.
 //
 // Returns nothing.
@@ -92,15 +92,15 @@ export function init(cacheDir=null, skipRegister=false) {
 
 // Public: Initializes the electron-compile library. Once this method is called,
 //         all JavaScript and CSS that is loaded will now be first transpiled, in
-//         both the browser and renderer processes. 
+//         both the browser and renderer processes.
 //
-//         Note that because of limitations in Electron, this does **not** apply 
+//         Note that because of limitations in Electron, this does **not** apply
 //         to WebView or Browser preload scripts - call init again at the top of
 //         these scripts to set everything up again.
 //
 //  options: an options {Object} with the following keys:
 //
-//     :cacheDir - The directory to cache compiled JS and CSS to. If not given, 
+//     :cacheDir - The directory to cache compiled JS and CSS to. If not given,
 //                 one will be generated from the Temp directory.
 //
 //     :skipRegister - Do not register with the node.js module system. For testing.
@@ -120,7 +120,7 @@ export function init(cacheDir=null, skipRegister=false) {
 export function initWithOptions(options={}) {
   let {cacheDir, skipRegister, compilers} = options;
   if (lastCacheDir === cacheDir && availableCompilers) return;
-  
+
   if (!cacheDir) {
     let tmpDir = process.env.TEMP || process.env.TMPDIR || '/tmp';
     let hash = require('crypto').createHash('md5').update(process.execPath).digest('hex');
@@ -128,7 +128,7 @@ export function initWithOptions(options={}) {
     cacheDir = path.join(tmpDir, `compileCache_${hash}`);
     mkdirp.sync(cacheDir);
   }
-  
+
   availableCompilers = compilers || createAllCompilers(options.compilerOpts);
   lastCacheDir = cacheDir;
 
@@ -139,13 +139,39 @@ export function initWithOptions(options={}) {
 
   // If we're not an Electron browser process, bail
   if (!process.type || process.type !== 'browser') return;
-  
+
   const app = require('app');
   const initProtoHook = () => initializeProtocolHook(availableCompilers, cacheDir);
-  
+
   if (app.isReady()) {
     initProtoHook();
   } else {
     app.on('ready', initProtoHook);
   }
+}
+
+// Public: Returns information about the current compilers' configured options.
+// This information can be used to create a read-only version of a compiler that
+// only returns cached / precompiled information.
+//
+// compilers: (optional) - An {Array} of objects conforming to {CompileCache}
+//                         that will be tried in-order to compile code. You must
+//                         call init() first if this parameter is null.
+//
+// Returns an {Object} whose keys are the joined list of extensions from the
+// compilers (i.e. ['js', 'jsx'] has a key of 'js,jsx'), and whose value is the
+// result of getCompilerInformation (i.e. an {Object} representing the compiler
+// configuration for that compiler).
+export function collectCompilerInformation(compilers=null) {
+  compilers = compilers || availableCompilers;
+  if (!compilers) {
+    throw new Error("Call init() first or pass in an Array to the compilers parameter");
+  }
+
+  return _.reduce(compilers, (acc,x) => {
+    let key = x.getExtensions().join(',');
+    acc[key] = x.getCompilerInformation();
+
+    return acc;
+  }, {});
 }
