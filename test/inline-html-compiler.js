46b3c5ce9a3b487dc31b93892380dbc0bf59f9ae
Paul Betts <paul@paulbetts.org>
2015-07-20 00:05:58 -0700

    More test cases

--- test/inline-html-compiler.js
+++ test/inline-html-compiler.js
@@ -10,40 +10,47 @@ const BabelCompiler = global.importCompilerByExtension('js');
 const CoffeescriptCompiler = global.importCompilerByExtension('coffee');
 const InlineHtmlCompiler = global.importCompilerByExtension('html');
 
+const validInputs = [
+  'inline-valid.html',
+  'inline-valid-2.html',
+]
+
 describe('The inline HTML compiler', function() {
-  it('should compile the valid fixture', function() {
-    let compilers = _.map([LessCompiler, BabelCompiler, CoffeescriptCompiler], (Klass) => {
-      let ret = new Klass();
-      ret.setCacheDirectory(null);
-      return ret;
-    });
-    
-    let fixture = new InlineHtmlCompiler((sourceCode, filePath) => {
-      let compiler = _.find(compilers, (x) => x.shouldCompileFile(filePath, sourceCode));
-      if (!compiler) {
-        throw new Error("Couldn't find a compiler for " + filePath);
-      }
+  
+  _.each(validInputs, (inputFile) => {
+    it('should compile the valid fixture ' + inputFile, function() {
+      let compilers = _.map([LessCompiler, BabelCompiler, CoffeescriptCompiler], (Klass) => {
+        let ret = new Klass();
+        ret.setCacheDirectory(null);
+        return ret;
+      });
       
-      return compiler.loadFile(null, filePath, true, sourceCode);
-    });
-    
-    fixture.setCacheDirectory(null);
-    
-    let input = path.join(__dirname, '..', 'test', 'fixtures', 'inline-valid.html');
-    let result = fixture.loadFile(null, input, true);
+      let fixture = new InlineHtmlCompiler((sourceCode, filePath) => {
+        let compiler = _.find(compilers, (x) => x.shouldCompileFile(filePath, sourceCode));
+        if (!compiler) {
+          throw new Error("Couldn't find a compiler for " + filePath);
+        }
         
-    console.log(result);
-    expect(result.length > 0).to.be.ok;
-    
-    let $ = cheerio.load(result);
-    let tags = $('script');
-    expect(tags.length === 3).to.be.ok
-    
-    $('script').map((__, el) => {
-      let text = $(el).text();
-      if (!text || text.length < 2) return;
+        return compiler.loadFile(null, filePath, true, sourceCode);
+      });
       
-      expect(_.find(text.split('\n'), (l) => l.match(/sourceMappingURL/))).to.be.ok;
+      fixture.setCacheDirectory(null);
+      
+      let input = path.join(__dirname, '..', 'test', 'fixtures', inputFile);
+      let result = fixture.loadFile(null, input, true);
+          
+      expect(result.length > 0).to.be.ok;
+      
+      let $ = cheerio.load(result);
+      let tags = $('script');
+      expect(tags.length > 0).to.be.ok
+      
+      $('script').map((__, el) => {
+        let text = $(el).text();
+        if (!text || text.length < 2) return;
+        
+        expect(_.find(text.split('\n'), (l) => l.match(/sourceMappingURL/))).to.be.ok;
+      });
     });
-  });
+  })
 });
