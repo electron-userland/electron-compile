df4e76d526472280d8c35b022ff3cf3463456bda
Paul Betts <paul@paulbetts.org>
2015-08-14 07:57:28 -0700

    Fix the legions of bugs that x-require has

--- src/x-require.js
+++ src/x-require.js
@@ -1,29 +1,39 @@
 import _ from 'lodash';
 import url from 'url';
 
+function requireModule(href) {
+  let filePath = href;
+  
+  if (filePath.match(/^file:/i)) {
+    let theUrl = url.parse(filePath);
+    filePath = decodeURIComponent(theUrl.pathname);
+
+    if (process.platform === 'win32') {
+      filePath = filePath.slice(1);
+    }
+  }
+  
+  // NB: We don't do any path canonicalization here because we rely on
+  // InlineHtmlCompiler to have already converted any relative paths that
+  // were used with x-require into absolute paths.
+  require(filePath);
+}
+
 export default (() => {
   if (process.type !== 'renderer' || !window || !window.document) return null;
   
   let proto = _.extend(Object.create(HTMLElement.prototype), {
-    attributeChangedCallback: function(attrName, oldVal, newVal) {
-      if (attrName !== 'href') return;
-      let filePath = newVal;
-      
-      if (newVal.match(/^file:/i)) {
-        let theUrl = url.parse(newVal);
-        filePath = decodeURIComponent(theUrl.pathname);
-
-        if (process.platform === 'win32') {
-          filePath = filePath.slice(1);
-        }
+    createdCallback: function() {
+      let href = this.getAttribute('src');
+      if (href && href.length > 0) {
+        requireModule(href);
       }
-      
-      // NB: We don't do any path canonicalization here because we rely on
-      // InlineHtmlCompiler to have already converted any relative paths that
-      // were used with x-require into absolute paths.
-      require(filePath);
+    }, 
+    attributeChangedCallback: function(attrName, oldVal, newVal) {
+      if (attrName !== 'src') return;
+      requireModule(newVal);
     }
   });
 
-  return document.registerElement('x-require', proto);
+  return document.registerElement('x-require', { prototype: proto });
 }());
