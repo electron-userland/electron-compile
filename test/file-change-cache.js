1dd12d4465dcf5841a51292795247bb59aef6205
Paul Betts <paul@paulbetts.org>
2015-10-27 10:50:55 -0700

    Sketch out some tests that should pass

---
+++ test/file-change-cache.js
@@ -0,0 +1,35 @@
+
+describe('The file changed cache', function() {
+  it("Correctly computes a file hash for a canned file", function() {
+    expect(false).to.be.ok;
+  });
+
+  it("Doesn't rerun the file hash if you ask for it twice", function() {
+    expect(false).to.be.ok;
+  });
+
+  it("Throws on cache misses in production mode", function() {
+    expect(false).to.be.ok;
+  });
+
+  it("Successfully saves and loads its cache information", function() {
+    expect(false).to.be.ok;
+  });
+
+
+  it("Detects changes to files and reruns hash", function() {
+    expect(false).to.be.ok;
+  });
+
+  it("Successfully finds if a file has a source map", function() {
+    expect(false).to.be.ok;
+  });
+
+  it("Successfully finds if a file is minified", function() {
+    expect(false).to.be.ok;
+  });
+
+  it("Successfully finds if a file is in node_modules", function() {
+    expect(false).to.be.ok;
+  });
+});
