0dad703a85da0e6ec1536ad5602889ffb50e0887
Paul Betts <paul@paulbetts.org>
2015-10-28 13:56:01 -0700

    Implement erry'thing

--- src/file-change-cache.js
+++ src/file-change-cache.js
@@ -1,12 +1,14 @@
-import pify from 'pify';
 import fs from 'fs';
 import zlib from 'zlib';
+import crypto from 'crypto';
+import pify from 'pify';
 
 const pfs = pify(fs);
 const pzlib = pify(zlib);
 
 export default class FileChangedCache {
-  constructor(failOnCacheMiss) {
+  constructor(appRoot, failOnCacheMiss) {
+    this.appRoot = appRoot;
     this.failOnCacheMiss = failOnCacheMiss;
     this.changeCache = {};
   }
@@ -16,12 +18,79 @@ export default class FileChangedCache {
     let buf = await pfs.readFile(file);
     
     ret.changeCache = JSON.parse(await pzlib.gunzip(buf));
+    return ret;
+  }
+  
+  async getHashForPath(absoluteFilePath) {
+    let cacheKey = this.appRoot ? absoluteFilePath.replace(this.appRoot, '') : absoluteFilePath;
+    let cacheEntry = this.changeCache[cacheKey];
+    
+    if (this.failOnCacheMiss) {
+      if (!cacheEntry) throw new Error(`Asked for ${absoluteFilePath} but it was not precompiled!`);
+      return cacheEntry.info;
+    }
+        
+    let stat = await pfs.stat(absoluteFilePath);
+    let ctime = stat.ctime.getTime();
+    let size = stat.size;
+    if (!stat || !stat.isFile()) throw new Error(`Can't stat ${absoluteFilePath}`);
+    
+    if (cacheEntry) {
+      if (cacheEntry.ctime >= ctime && cacheEntry.size === size) {
+        return cacheEntry.info;
+      }
+      
+      delete this.changeCache.cacheEntry;
+    }
+    
+    let {digest, sourceCode} = await this.calculateHashForFile(absoluteFilePath);
+    
+    let info = {
+      hash: digest,
+      isMinified: FileChangedCache.contentsAreMinified(sourceCode),
+      isInNodeModules: FileChangedCache.isInNodeModules(absoluteFilePath),
+      hasSourceMap: FileChangedCache.hasSourceMap(sourceCode)
+    };
+    
+    this.changeCache[cacheKey] = { ctime, size, info };
+    return info;
   }
 
-  getHashForPath(filePath) {
-  }
-
-  getHashForPathSync(filePath) {
+  getHashForPathSync(absoluteFilePath) {
+    // NB: Don't ever make patches to this method, patch the async version then re-port it
+    // (i.e. every 'await', remove the await and add 'Sync' to the method call name)
+    let cacheKey = this.appRoot ? absoluteFilePath.replace(this.appRoot, '') : absoluteFilePath;
+    let cacheEntry = this.changeCache[cacheKey];
+    
+    if (this.failOnCacheMiss) {
+      if (!cacheEntry) throw new Error(`Asked for ${absoluteFilePath} but it was not precompiled!`);
+      return cacheEntry.info;
+    }
+        
+    let stat = pfs.statSync(absoluteFilePath);
+    let ctime = stat.ctime.getTime();
+    let size = stat.size;
+    if (!stat || !stat.isFile()) throw new Error(`Can't stat ${absoluteFilePath}`);
+    
+    if (cacheEntry) {
+      if (cacheEntry.ctime >= ctime && cacheEntry.size === size) {
+        return cacheEntry.info;
+      }
+      
+      delete this.changeCache.cacheEntry;
+    }
+    
+    let {digest, sourceCode} = this.calculateHashForFileSync(absoluteFilePath);
+    
+    let info = {
+      hash: digest,
+      isMinified: FileChangedCache.contentsAreMinified(sourceCode),
+      isInNodeModules: FileChangedCache.isInNodeModules(absoluteFilePath),
+      hasSourceMap: FileChangedCache.hasSourceMap(sourceCode)
+    };
+    
+    this.changeCache[cacheKey] = { ctime, size, info };
+    return info;
   }
 
   async save(filePath) {
@@ -29,19 +98,20 @@ export default class FileChangedCache {
     await pfs.writeFile(filePath, buf);
   }
   
-  static async isMinified(filePath) {
-    let fd = await pfs.open(filePath, 'r');
+  async calculateHashForFile(absoluteFilePath) {
+    let sourceCode = await pfs.readFile(absoluteFilePath, 'utf8');
+    let digest = crypto.createHash('sha1').update(sourceCode, 'utf8').digest('hex');
     
-    try {
-      let buf = new Buffer(4096);
-      let len = await pfs.read(fd, buf, 0, 4096, null);
-      
-      return FileChangedCache.contentsAreMinified(buf.toString('utf8', 0, len));
-    } finally {
-      fs.closeSync(fd);
-    }
+    return {sourceCode, digest};
   }
-
+    
+  calculateHashForFileSync(absoluteFilePath) {
+    let sourceCode = pfs.readFileSync(absoluteFilePath, 'utf8');
+    let digest = crypto.createHash('sha1').update(sourceCode, 'utf8').digest('hex');
+    
+    return {sourceCode, digest};
+  }
+  
   static contentsAreMinified(source) {
     let length = source.length;
     if (length > 1024) length = 1024;
@@ -63,11 +133,10 @@ export default class FileChangedCache {
   }
 
   static isInNodeModules(filePath) {
-    // If we're in node_modules or in Electron core code, we're gonna punt
-    if (filePath.match(/[\\\/]node_modules[\\\/]/i) || filePath.match(/[\\\/]atom\.asar/)) return false;
+    return !!(filePath.match(/[\\\/]node_modules[\\\/]/i) || filePath.match(/[\\\/]atom\.asar/));
   }
 
-  static hasSourceMap(filePath) {
-    return false;
+  static hasSourceMap(sourceCode) {
+    return sourceCode.lastIndexOf('//# sourceMap') > sourceCode.lastIndexOf('\n');
   }
 }
--- test/file-change-cache.js
+++ test/file-change-cache.js
@@ -1,34 +1,50 @@
 require('./support.js');
 
 import FileChangeCache from '../lib/file-change-cache';
-import pify from 'pify';
 import path from 'path';
 import fs from 'fs';
+import pify from 'pify';
 const pfs = pify(fs);
 
-describe('The file changed cache', function() {
+describe.only('The file changed cache', function() {
   beforeEach(function() {
     this.fixture = new FileChangeCache();
   });
 
   it("Correctly computes a file hash for a canned file", async function() {
     const expectedInfo = {
-      hash: 'SOME HASH',
+      hash: '4a92e95074156e8b46869519c43ddf10b59299a4',
       hasSourceMap: false,
-      insideNodeModules: false
+      isInNodeModules: false,
+      isMinified: false
     };
 
-    let input = path.join(__dirname, '..', 'test', 'fixtures', 'valid.js');
+    let input = path.resolve(__dirname, '..', 'test', 'fixtures', 'valid.js');
     let result = await this.fixture.getHashForPath(input);
 
     expect(result).to.deep.equal(expectedInfo);
   });
-
-  it("Doesn't rerun the file hash if you ask for it twice", function() {
+  
+  it("Correctly computes a file hash for a canned file syncronously", function() {
     const expectedInfo = {
-      hash: 'SOME HASH',
+      hash: '4a92e95074156e8b46869519c43ddf10b59299a4',
       hasSourceMap: false,
-      insideNodeModules: false
+      isInNodeModules: false,
+      isMinified: false
+    };
+
+    let input = path.resolve(__dirname, '..', 'test', 'fixtures', 'valid.js');
+    let result = this.fixture.getHashForPathSync(input);
+
+    expect(result).to.deep.equal(expectedInfo);
+  });
+
+  it("Doesn't rerun the file hash if you ask for it twice", async function() {
+    const expectedInfo = {
+      hash: '4a92e95074156e8b46869519c43ddf10b59299a4',
+      hasSourceMap: false,
+      isInNodeModules: false,
+      isMinified: false
     };
 
     let input = path.join(__dirname, '..', 'test', 'fixtures', 'valid.js');
@@ -36,7 +52,7 @@ describe('The file changed cache', function() {
 
     expect(result).to.deep.equal(expectedInfo);
 
-    this.fixture.replaceThisMethod = () => throw new Error("bar");
+    this.fixture.calculateHashForFile = () => Promise.reject(new Error("Didn't work"));
     result = await this.fixture.getHashForPath(input);
 
     expect(result).to.deep.equal(expectedInfo);
@@ -49,9 +65,9 @@ describe('The file changed cache', function() {
     expect(this.fixture.getHashForPath(input)).to.eventually.throw(Error);
   });
 
-  it("Successfully saves and loads its cache information", function() {
+  it("Successfully saves and loads its cache information", async function() {
     let input = path.join(__dirname, '..', 'test', 'fixtures', 'valid.js');
-    let result = await this.fixture.getHashForPath(input);
+    await this.fixture.getHashForPath(input);
 
     let targetCache = path.join(__dirname, 'fileChangeCache1.json.gz');
 
@@ -60,52 +76,81 @@ describe('The file changed cache', function() {
 
       this.fixture = await FileChangeCache.loadFromFile(targetCache);
 
-      this.fixture.replaceThisMethod = () => throw new Error("bar");
-      let result = await this.fixture.getHashForPath(input);
+      this.fixture.calculateHashForFile = () => Promise.reject(new Error("Didn't work"));
+      await this.fixture.getHashForPath(input);
     } finally {
       fs.unlinkSync(targetCache);
     }
   });
 
-  it("Detects changes to files and reruns hash", function() {
+  it("Detects changes to files and reruns hash", async function() {
     const expectedInfo = {
-      hash: 'SOME HASH',
+      hash: '4a92e95074156e8b46869519c43ddf10b59299a4',
       hasSourceMap: false,
-      insideNodeModules: false
+      isInNodeModules: false,
+      isMinified: false
     };
 
-    let input = path.join(__dirname, '..', 'test', 'fixtures', 'valid.js');
-    let result = await this.fixture.getHashForPath(input);
+    let realInput = path.join(__dirname, '..', 'test', 'fixtures', 'valid.js');
+    let input = path.join(__dirname, 'tempfile.tmp');
+    let contents = await pfs.readFile(realInput);
+    await pfs.writeFile(input, contents);
+    
+    let stat1 = await pfs.stat(realInput);
+    let stat2 = await pfs.stat(input);
+    expect(stat1.size).to.equal(stat2.size);
+    
+    try {
+      let result = await this.fixture.getHashForPath(input);
 
-    expect(result).to.deep.equal(expectedInfo);
+      expect(result).to.deep.equal(expectedInfo);
+      
+      let fd = await pfs.open(input, 'a');
+      await pfs.write(fd, '\n\n\n\n');
+      await pfs.close(fd);
+      
+      let realCalc = this.fixture.calculateHashForFile;
+      let hasCalledCalc = false;
+      
+      this.fixture.calculateHashForFile = function(...args) {
+        hasCalledCalc = true;
+        return realCalc(...args);
+      };
+      
+      result = await this.fixture.getHashForPath(input);
 
-    this.fixture.replaceThisMethod = chai.spy(this.fixture.replaceThisMethod);
-    result = await this.fixture.getHashForPath(input);
-
-    expect(result).not.to.deep.equal(expectedInfo);
-    expect(this.fixture.replaceThisMethod).to.have.been.called();
+      expect(result).not.to.deep.equal(expectedInfo);
+      expect(hasCalledCalc).to.be.ok;
+    } finally {
+      fs.unlinkSync(input);
+    }
   });
 
-  it("Successfully finds if a file has a source map", function() {
-    expect(false).to.be.ok;
-
+  it("Successfully finds if a file has a source map", async function() {
     let input = path.join(__dirname, '..', 'test', 'fixtures', 'source_map.js');
     let result = await this.fixture.getHashForPath(input);
 
     expect(result.hasSourceMap).to.be.ok;
   });
+  
+  it("Successfully finds if a file has a source map synchronously", function() {
+    let input = path.join(__dirname, '..', 'test', 'fixtures', 'source_map.js');
+    let result = this.fixture.getHashForPathSync(input);
 
-  it("Successfully finds if a file is minified", function() {
+    expect(result.hasSourceMap).to.be.ok;
+  });
+
+  it("Successfully finds if a file is minified", async function() {
     let input = path.join(__dirname, '..', 'test', 'fixtures', 'minified.js');
     let result = await this.fixture.getHashForPath(input);
 
     expect(result.isMinified).to.be.ok;
   });
 
-  it("Successfully finds if a file is in node_modules", function() {
+  it("Successfully finds if a file is in node_modules", async function() {
     let input = path.join(__dirname, '..', 'node_modules', 'electron-compilers', 'package.json');
     let result = await this.fixture.getHashForPath(input);
 
-    expect(result.insideNodeModules).to.be.ok;
+    expect(result.isInNodeModules).to.be.ok;
   });
 });
