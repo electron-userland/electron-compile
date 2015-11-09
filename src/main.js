c31af114fe4dbd1ed2504e7d0a7753cb18324af1
Paul Betts <paul@paulbetts.org>
2015-11-08 22:54:46 -0800

    Rig some Babel 6 changes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 
 const filenames = [
-  'css/less',
+//  'css/less',
   'js/babel-ng',
   'js/coffeescript-ng',
   'js/typescript-ng',
-  'inline-html'
+  //'inline-html'
 ];
 
-export default _.map(filenames, (x) => require('./' + x));
+module.exports = _.map(filenames, (x) => require('./' + x).default);
