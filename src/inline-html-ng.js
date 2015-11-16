1532c9442f18f0f5d71e85bc7c3e32c177c391e5
Paul Betts <paul@paulbetts.org>
2015-11-16 15:37:30 -0800

    Make inline HTML compiler truly async

--- src/inline-html-ng.js
+++ src/inline-html-ng.js
@@ -6,8 +6,10 @@ const inputMimeTypes = ['text/html'];
 let cheerio = null;
 
 export default class InlineHtmlCompiler extends CompilerBase {
-  constructor(compileBlockSync) {
+  constructor(compileBlock, compileBlockSync) {
     super();
+
+    this.compileBlock = compileBlock;
     this.compileBlockSync = compileBlockSync;
   }
 
@@ -23,18 +25,33 @@ export default class InlineHtmlCompiler extends CompilerBase {
     return [];
   }
 
+  async eachAsync(nodes, selector) {
+    let acc = [];
+    nodes.each((i, el) => {
+      let promise = selector(i,el);
+      if (!promise) return false;
+
+      acc.push(promise);
+      return true;
+    });
+
+    await Promise.all(acc);
+  }
+
   async compile(sourceCode, filePath, compilerContext) {
     cheerio = cheerio || require('cheerio');
     let $ = cheerio.load(sourceCode);
+    let toWait = [];
 
-    $('style').map((i, el) => {
+    let that = this;
+    toWait.push(this.eachAsync($('style'), async (i, el) => {
       let mimeType = $(el).attr('type');
 
-      $(el).text(this.compileBlockSync($(el).text(), filePath, mimeType, 'style'));
+      $(el).text(await that.compileBlock($(el).text(), filePath, mimeType, 'style'));
       $(el).attr('type', 'text/css');
-    });
+    }));
 
-    $('script').map((i, el) => {
+    toWait.push(this.eachAsync($('script'), async (i, el) => {
       let src = $(el).attr('src');
       if (src && src.length > 2) {
         $(el).attr('src', InlineHtmlCompiler.fixupRelativeUrl(src));
@@ -43,9 +60,9 @@ export default class InlineHtmlCompiler extends CompilerBase {
 
       let mimeType = $(el).attr('type');
 
-      $(el).text(this.compileBlockSync($(el).text(), filePath, mimeType, 'script'));
+      $(el).text(await that.compileBlock($(el).text(), filePath, mimeType, 'script'));
       $(el).attr('type', 'application/javascript');
-    });
+    }));
 
     $('link').map((i, el) => {
       let href = $(el).attr('href');
@@ -67,6 +84,8 @@ export default class InlineHtmlCompiler extends CompilerBase {
         $(el).text(`${e.message}\n${e.stack}`);
       }
     });
+
+    await Promise.all(toWait);
 
     return {
       code: $.html(),
