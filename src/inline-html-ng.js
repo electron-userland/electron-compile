246801dea0612ad18920134d5f00c28b8bcfb8ae
Paul Betts <paul@paulbetts.org>
2015-11-16 15:46:40 -0800

    Fix up sync version

--- src/inline-html-ng.js
+++ src/inline-html-ng.js
@@ -25,7 +25,7 @@ export default class InlineHtmlCompiler extends CompilerBase {
     return [];
   }
 
-  async eachAsync(nodes, selector) {
+  async each(nodes, selector) {
     let acc = [];
     nodes.each((i, el) => {
       let promise = selector(i,el);
@@ -38,20 +38,29 @@ export default class InlineHtmlCompiler extends CompilerBase {
     await Promise.all(acc);
   }
 
+  eachSync(nodes, selector) {
+    // NB: This method is here just so it's easier to mechanically
+    // translate the async compile to compileSync
+    return nodes.each((i,el) => {
+      selector(i,el);
+      return true;
+    });
+  }
+
   async compile(sourceCode, filePath, compilerContext) {
     cheerio = cheerio || require('cheerio');
     let $ = cheerio.load(sourceCode);
     let toWait = [];
 
     let that = this;
-    toWait.push(this.eachAsync($('style'), async (i, el) => {
+    toWait.push(this.each($('style'), async (i, el) => {
       let mimeType = $(el).attr('type');
 
       $(el).text(await that.compileBlock($(el).text(), filePath, mimeType, 'style'));
       $(el).attr('type', 'text/css');
     }));
 
-    toWait.push(this.eachAsync($('script'), async (i, el) => {
+    toWait.push(this.each($('script'), async (i, el) => {
       let src = $(el).attr('src');
       if (src && src.length > 2) {
         $(el).attr('src', InlineHtmlCompiler.fixupRelativeUrl(src));
@@ -103,6 +112,54 @@ export default class InlineHtmlCompiler extends CompilerBase {
 
   compileSync(sourceCode, filePath, compilerContext) {
     cheerio = cheerio || require('cheerio');
+    let $ = cheerio.load(sourceCode);
+
+    let that = this;
+    this.eachSync($('style'), (i, el) => {
+      let mimeType = $(el).attr('type');
+
+      $(el).text(that.compileBlockSync($(el).text(), filePath, mimeType, 'style'));
+      $(el).attr('type', 'text/css');
+    });
+
+    this.eachSync($('script'), (i, el) => {
+      let src = $(el).attr('src');
+      if (src && src.length > 2) {
+        $(el).attr('src', InlineHtmlCompiler.fixupRelativeUrl(src));
+        return;
+      }
+
+      let mimeType = $(el).attr('type');
+
+      $(el).text(that.compileBlockSync($(el).text(), filePath, mimeType, 'script'));
+      $(el).attr('type', 'application/javascript');
+    });
+
+    $('link').map((i, el) => {
+      let href = $(el).attr('href');
+      if (href && href.length > 2) { $(el).attr('href', InlineHtmlCompiler.fixupRelativeUrl(href)); }
+    });
+
+    $('x-require').map((i, el) => {
+      let src = $(el).attr('src');
+
+      // File URL? Bail
+      if (src.match(/^file:/i)) return;
+
+      // Absolute path? Bail.
+      if (src.match(/^([\/]|[A-Za-z]:)/i)) return;
+
+      try {
+        $(el).attr('src', path.resolve(path.dirname(filePath), src));
+      } catch (e) {
+        $(el).text(`${e.message}\n${e.stack}`);
+      }
+    });
+
+    return {
+      code: $.html(),
+      mimeType: 'text/html'
+    };
   }
 
   getCompilerVersion() {
