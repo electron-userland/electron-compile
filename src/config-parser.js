510fc8761adfd5186754be88c34206b69c481c78
Paul Betts <paul@paulbetts.org>
2015-12-27 20:52:47 -0800

    Move createCompilers to configuration parser

--- src/config-parser.js
+++ src/config-parser.js
@@ -6,6 +6,10 @@ import pify from 'pify';
 const pfs = pify(fs);
 const d = require('debug')('electron-compile:config-parser');
 
+// NB: We intentionally delay-load this so that in production, you can create
+// cache-only versions of these compilers
+let allCompilerClasses = null;
+
 export async function createCompilerHostFromConfiguration(info) {
 }
 
@@ -16,4 +20,48 @@ export function createCompilerHostFromConfigFile(file) {
 }
 
 export function createCompilerHostFromProjectRoot(rootDir) {
+}
+
+// Public: Allows you to create new instances of all compilers that are
+// supported by electron-compile and use them directly. Currently supports
+// Babel, CoffeeScript, TypeScript, LESS, and Sass/SCSS.
+//
+// Returns an {Object} whose Keys are MIME types, and whose values are objects
+// which conform to {CompilerBase}.
+export function createCompilers() {
+  if (!allCompilerClasses) {
+    // First we want to see if electron-compilers itself has been installed with
+    // devDependencies. If that's not the case, check to see if
+    // electron-compilers is installed as a peer dependency (probably as a
+    // devDependency of the root project).
+    const locations = ['electron-compilers', '../../electron-compilers'];
+
+    for (let location of locations) {
+      try {
+        allCompilerClasses = require(location);
+      } catch (e) {
+        // Yolo
+      }
+    }
+
+    if (!allCompilerClasses) {
+      throw new Error("Electron compilers not found but were requested to be loaded");
+    }
+  }
+
+  let ret = {};
+  let instantiatedClasses = _.map(allCompilerClasses, (Klass) => {
+    if ('createFromCompilers' in Klass) {
+      return Klass.createFromCompilers(ret);
+    } else {
+      return new Klass();
+    }
+  });
+
+  return _.reduce(instantiatedClasses, (acc,x) => {
+    let Klass = Object.getPrototypeOf(x).constructor;
+
+    for (let type of Klass.getInputMimeTypes()) { acc[type] = x; }
+    return acc;
+  }, {});
 }
