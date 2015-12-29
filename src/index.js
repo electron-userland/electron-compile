6ea4445f607d3a9675b669968068d9aa05093483
Paul Betts <paul@paulbetts.org>
2015-12-29 10:59:44 -0800

    Fix package oopses

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'babel-polyfill';
 import _ from 'lodash';
 
-import configParser from './config-parser';
+import * as configParser from './config-parser';
 
 import CompilerHost from './compiler-host';
 import FileChangedCache from './file-change-cache';
