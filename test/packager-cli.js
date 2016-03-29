829debd5fd711e21be711b1d27baaab461ae9ca7
Paul Betts <paul@paulbetts.org>
2016-03-28 21:03:15 -0700

    Add a test for finding electron-compile

--- test/packager-cli.js
+++ test/packager-cli.js
@@ -6,7 +6,7 @@ import path from 'path';
 import rimraf from 'rimraf';
 import mkdirp from 'mkdirp';
 
-import {main} from '../src/packager-cli';
+import {packagerMain} from '../src/packager-cli';
 
 const d = require('debug')('test:packager-cli');
 
@@ -29,13 +29,31 @@ describe.only('the packager CLI', function() {
 
     // NB: The first two elements are dummies to fake out what would normally
     // be the path to node and the path to the script
-    await main(['', '', '--platform', 'win32', '--arch', 'ia32', '--out', this.tempCacheDir, inputApp]);
+    await packagerMain(['', '', '--platform', 'win32', '--arch', 'all', '--out', this.tempCacheDir, inputApp]);
 
     const toFind = ['node.dll', 'resources', 'resources/app/src/main.coffee'];
     let cacheDir = this.tempCacheDir;
 
     _.each(toFind, (name) => {
       let file = path.resolve(cacheDir, 'mp3-encoder-demo-win32-ia32', name);
+
+      d(`Looking for ${file}`);
+      expect(sfs.statSync(file)).to.be.ok;
+    });
+  });
+
+  it.only('should run electron-compile', async function() {
+    let inputApp = path.resolve(__dirname, 'electron-app');
+
+    // NB: The first two elements are dummies to fake out what would normally
+    // be the path to node and the path to the script
+    await packagerMain(['', '', '--platform', 'win32', '--arch', 'x64', '--out', this.tempCacheDir, inputApp]);
+
+    const toFind = ['resources/app/.cache', 'resources/app/.compilerc'];
+    let cacheDir = this.tempCacheDir;
+
+    _.each(toFind, (name) => {
+      let file = path.resolve(cacheDir, 'mp3-encoder-demo-win32-x64', name);
 
       d(`Looking for ${file}`);
       expect(sfs.statSync(file)).to.be.ok;
