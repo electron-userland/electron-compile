ae75744f0ba6f7a29d363ac749d8ffb8aee55623
Paul Betts <paul@paulbetts.org>
2016-04-03 23:48:06 -0700

    Try harder to find BabelJS preset modules, even if invoked via Mocha

--- src/js/babel.js
+++ src/js/babel.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import path from 'path';
 import {CompilerBase} from '../compiler-base';
 
 const mimeTypes = ['text/jsx', 'application/javascript'];
 let babel = null;
+
 
 /**
  * @access private
@@ -29,11 +31,24 @@ export default class BabelCompiler extends CompilerBase {
   // installed in it. Instead, we try to load from our entry point's node_modules
   // directory (i.e. Grunt perhaps), and if it doesn't work, just keep going.
   attemptToPreload(names, prefix) {
-    try {
-      return _.map(names, (x) => require.main.require(`babel-${prefix}-${x}`));
-    } catch (e) {
-      return null;
+    const preloadStrategies = [
+      () => _.map(names, (x) => require.main.require(`babel-${prefix}-${x}`)),
+      () => {
+        let nodeModulesAboveUs = path.resolve(__dirname, '..', '..', '..');
+        return _.map(names, (x) => require(path.join(nodeModulesAboveUs, `babel-${prefix}-${x}`)));
+      },
+      () => _.map(names, (x) => require(`babel-${prefix}-${x}`))
+    ];
+
+    for (let strategy of preloadStrategies) {
+      try {
+        return strategy();
+      } catch (e) {
+        continue;
+      }
     }
+
+    return null;
   }
 
   async compile(sourceCode, filePath, compilerContext) {
