e9799eb551e580932f9bb2ce9501a1108b62a7fe
Manuel Rueda <manuel.rueda.un@gmail.com>
2016-05-21 14:41:04 -0300

    update typescript-simple version and add option to avoid semantic check

--- src/js/typescript.js
+++ src/js/typescript.js
@@ -26,7 +26,7 @@ export default class TypeScriptCompiler extends SimpleCompilerBase {
     tss = tss || require('typescript-simple');
     
     // NB: Work around TypeScriptSimple modifying the options object
-    let compiler = new tss.TypeScriptSimple(_.assign({}, this.compilerOptions));
+    let compiler = new tss.TypeScriptSimple(_.assign({}, this.compilerOptions), this.compilerOptions.doSemanticChecks);
 
     return {
       code: compiler.compile(sourceCode, filePath),
