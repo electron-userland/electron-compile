dfad4439784c5196316f4a90432c816deafe8748
Lance Miller <lmiller@bamboohr.com>
2016-05-28 11:17:53 -0600

    add support for nib

--- src/css/stylus.js
+++ src/css/stylus.js
@@ -1,6 +1,7 @@
 import {SimpleCompilerBase} from '../compiler-base';
 import extend from 'lodash/object/extend';
 import {basename} from 'path';
+import nib from 'nib';
 
 const mimeTypes = ['text/stylus'];
 let stylusjs = null;
@@ -30,10 +31,24 @@ export default class StylusCompiler extends SimpleCompilerBase {
 
 		let code, error;
 
+		if (opts.import && !Array.isArray(opts.import)) {
+			opts.import = [opts.import];
+		}
+
+		if (opts.import && opts.import.indexOf('nib') >= 0) {
+			opts.use = opts.use || [];
+			if (!Array.isArray(opts.use)) {
+				opts.use = [opts.use];
+			}
+			opts.use.push(nib());
+		}
+
+		console.log(opts);
+
 		stylusjs.render(sourceCode, opts, (err, css) => {
-			error = err;
-			code = css;
-		});
+				error = err;
+				code = css;
+			});
 
 		if (error) {
 			throw error;
