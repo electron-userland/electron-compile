fb73cf28da05b5ab0492bec90f61ea6dc4d874fc
Paul Betts <paul@paulbetts.org>
2016-05-29 19:55:38 -0700

    Work around a bogus BabelJS bug in 6.9.2

--- src/babel-maybefill.js
+++ src/babel-maybefill.js
@@ -1,3 +1,4 @@
 if (!('regeneratorRuntime' in global)) {
   require('babel-polyfill');
+  global.regeneratorRuntime = global.regeneratorRuntime || require('regenerator-runtime');
 }
--- test/support.js
+++ test/support.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill';
+import '../src/babel-maybefill';
 
 import _ from 'lodash';
 
