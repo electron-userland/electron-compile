d53c90e3d0980f1dfa5fda6faef171e7d14b9a6a
Paul Betts <paul@paulbetts.org>
2016-08-12 14:59:47 -0700

    Support JSX in TypeScript

--- src/js/typescript.js
+++ src/js/typescript.js
@@ -1,7 +1,8 @@
 import {SimpleCompilerBase} from '../compiler-base';
 
-const inputMimeTypes = ['text/typescript'];
+const inputMimeTypes = ['text/typescript', 'text/tsx'];
 let tss = null;
+let ts = null;
 
 /**
  * @access private
@@ -23,6 +24,12 @@ export default class TypeScriptCompiler extends SimpleCompilerBase {
 
   compileSync(sourceCode, filePath) {
     tss = tss || require('typescript-simple');
+    ts = ts || require('typescript');
+    
+    let extraOpts = {target: ts.ScriptTarget.ES6};
+    if (filePath.match(/\.tsx$/i)) {
+      extraOpts.jsx = ts.JsxEmit.Preserve;
+    }
     
     // NB: Work around TypeScriptSimple modifying the options object
     let compiler = new tss.TypeScriptSimple(Object.assign({}, this.compilerOptions), this.compilerOptions.doSemanticChecks);
@@ -34,6 +41,6 @@ export default class TypeScriptCompiler extends SimpleCompilerBase {
   }
 
   getCompilerVersion() {
-    return require('typescript-simple/package.json').version;
+    return require('typescript/package.json').version;
   }
 }
