6bc4dbe8780fb8aae5b0ab7b1be85ffa9edbdac3
Sascha Gehlich <sascha@gehlich.us>
2017-01-15 21:13:07 +0100

    Fix windows support :(

--- src/css/sass.js
+++ src/css/sass.js
@@ -145,7 +145,36 @@ export default class SassCompiler extends CompilerBase {
             const stat = fs.statSync(file);
             if (!stat.isFile()) throw new Error(`${file} is not a file`);
           };
-          file = sass.findPathVariation(validator, filePath);
+          const variations = sass.getPathVariations(filePath);
+          file = variations
+            .map((file) => {
+              // Unfortunately, there's a bug in sass.js that seems to ignore the different
+              // path separators across platforms. We need to fix this.
+              if (process.platform === 'win32' && file[0] === '/') {
+                file = file.slice(1);
+              }
+
+              if (file[0] === '_') {
+                const parts = file.slice(1).split(path.sep);
+                const dir = parts.slice(0, -1).join(path.sep);
+                const fileName = parts.reverse()[0];
+                file = path.resolve(dir, '_' + fileName);
+              }
+              return file
+            })
+            .reduce(function(found, path) {
+              // Find the first variation that actually exists
+              if (found) return found;
+
+              try {
+                const stat = fs.statSync(path);
+                if (!stat.isFile()) return null;
+                return path;
+              } catch(e) {
+                return null;
+              }
+            }, null);
+
           if (file) {
             const content = fs.readFileSync(file, { encoding: 'utf8' });
             return sass.writeFile(file, content, () => {
