0ca92fe1269fa67445c1dfc9c02438f5eda6735c
Paul Betts <paul@paulbetts.org>
2017-02-14 13:14:35 -0800

    Export watchPath

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import CompilerHost from './compiler-host';
 import FileChangedCache from './file-change-cache';
 import CompileCache from './compile-cache';
 import {enableLiveReload} from './live-reload';
+import {watchPath} from './pathwatcher-rx';
 
 module.exports = Object.assign({},
   configParser,
-  { enableLiveReload, CompilerHost, FileChangedCache, CompileCache }
+  { enableLiveReload, watchPath, CompilerHost, FileChangedCache, CompileCache }
 );
