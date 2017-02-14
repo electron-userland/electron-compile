5592baa29926e386c28cd046b7c9948763b8d740
Paul Betts <paul@paulbetts.org>
2017-02-13 16:57:20 -0800

    Add hot-module reloading to TypeScript compiler

--- src/js/typescript.js
+++ src/js/typescript.js
@@ -1,5 +1,6 @@
 import {SimpleCompilerBase} from '../compiler-base';
 import path from 'path';
+import jsEscape from 'js-string-escape';
 
 const inputMimeTypes = ['text/typescript', 'text/tsx'];
 const d = require('debug')('electron-compile:typescript-compiler');
@@ -40,21 +41,81 @@ export default class TypeScriptCompiler extends SimpleCompilerBase {
     ts = ts || require('typescript');
     const options = this._getParsedConfigOptions(ts);
 
+    const isTsx = filePath.match(/\.tsx$/i);
     const transpileOptions = {
       compilerOptions: options,
       fileName: filePath.match(/\.(ts|tsx)$/i) ? path.basename(filePath) : null
     };
 
+    if (isTsx) {
+      sourceCode = this.addHotModuleLoadingRegistration(sourceCode, filePath, this.getExportsForFile(filePath, options));
+    }
+
     const output = ts.transpileModule(sourceCode, transpileOptions);
     const sourceMaps = output.sourceMapText ? output.sourceMapText : null;
 
-    d(output.diagnostics);
+    d(JSON.stringify(output.diagnostics));
 
+    let sourceMaps;
+    if (output.sourceMapText) {
+      sourceMaps = output.sourceMapText;
+    }
+
+
+    console.log(output.outputText);
     return {
       code: output.outputText,
       mimeType: this.outMimeType,
       sourceMaps
     };
+  }
+
+  addHotModuleLoadingRegistration(sourceCode, fileName, exports) {
+    if (exports.length < 1) return sourceCode;
+
+    let registrations = exports.map(x => 
+      `__REACT_HOT_LOADER__.register(${x}, "${x}", __FILENAME__);\n`
+    );
+
+    let tmpl = `
+${sourceCode}
+
+if (__REACT_HOT_LOADER__) {
+  const __FILENAME__ = "${jsEscape(fileName)}";
+  ${registrations}
+}`;
+
+    return tmpl;
+  }
+
+  getExportsForFile(fileName, tsOptions) {
+    let pg = ts.createProgram([fileName], tsOptions);
+    let c = pg.getTypeChecker();
+    let ret = [];
+
+    // Walk the tree to search for classes
+    let visit = (node) => {
+      if (!this.isNodeExported(node)) return;
+      
+      if (node.kind === ts.SyntaxKind.ClassDeclaration || node.kind === ts.SyntaxKind.FunctionDeclaration) {
+        ret.push(c.getSymbolAtLocation(node.name).getName());
+      }
+    };
+
+    let filePathWithForwardSlashes = fileName.replace(/[\\]/g, '/');
+    for (const sourceFile of pg.getSourceFiles()) {
+      if (sourceFile.fileName !== filePathWithForwardSlashes) {
+        continue;
+      }
+      
+      ts.forEachChild(sourceFile, visit);
+    }
+
+    return ret;
+  }
+
+  isNodeExported(node) {
+    return (node.flags & ts.NodeFlags.Export) !== 0 || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
   }
 
   getCompilerVersion() {
