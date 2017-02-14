e035dc0f42072d1ecc4433fe96739194acaf1f45
Paul Betts <paul@paulbetts.org>
2017-02-13 17:08:32 -0800

    We don't need this for Babel

--- src/js/babel.js
+++ src/js/babel.js
@@ -1,34 +1,19 @@
 import path from 'path';
-import fs from 'fs';
-import os from 'os';
-import detective from 'detective-es6';
-import resolve from 'resolve';
-import appModulePath from 'app-module-path';
-import {CompilerBase} from '../compiler-base';
+import {SimpleCompilerBase} from '../compiler-base';
 
 const mimeTypes = ['text/jsx', 'application/javascript'];
 let babel = null;
 
-let natives = process.binding('natives');
-
 /**
  * @access private
  */
-export default class BabelCompiler extends CompilerBase {
+export default class BabelCompiler extends SimpleCompilerBase {
   constructor() {
     super();
   }
 
   static getInputMimeTypes() {
     return mimeTypes;
-  }
-
-  async shouldCompileFile(fileName, compilerContext) {
-    return true;
-  }
-
-  async determineDependentFiles(sourceCode, filePath, compilerContext) {
-    return this.determineDependentFilesSync(sourceCode, filePath, compilerContext);
   }
 
   // NB: This method exists to stop Babel from trying to load plugins from the
@@ -63,7 +48,7 @@ export default class BabelCompiler extends CompilerBase {
     return null;
   }
 
-  transformCode(sourceCode, filePath) {
+  compileSync(sourceCode, filePath, compilerContext) {
     babel = babel || require('babel-core');
 
     let opts = Object.assign({}, this.compilerOptions, {
@@ -82,93 +67,13 @@ export default class BabelCompiler extends CompilerBase {
       if (presets && presets.length === opts.presets.length) opts.presets = presets;
     }
     const output = babel.transform(sourceCode, opts);
-    const sourceMapObject = output.map;
-
-    let sourceMaps;
-    if (sourceMapObject) {
-      sourceMapObject.sourcesContent && delete sourceMapObject.sourcesContent;
-      sourceMaps = JSON.stringify(sourceMapObject);
-    }
+    const sourceMaps = output.map ? JSON.stringify(output.map) : null;
 
     return {
       code: output.code,
       mimeType: 'application/javascript',
       sourceMaps
     };
-  }
-
-  async compile(sourceCode, filePath, compilerContext) {
-    return this.transformCode(sourceCode, filePath);
-  }
-
-  shouldCompileFileSync(fileName, compilerContext) {
-    return true;
-  }
-
-  walkUntilPackageJsonFound(directory) {
-    let up = path.resolve(directory, '../');
-    let packageDotJson = path.resolve(up, 'package.json');
-    let exists = fs.existsSync(packageDotJson);
-
-    if (exists) {
-      return directory;
-    }
-
-    let rt = (os.platform == "win32") ? directory.split(path.sep)[0] : "/";
-
-    if (rt === directory) {
-      return false;
-    }
-
-    return this.walkUntilPackageJsonFound(up);
-  }
-
-  resolvePartial(partial, filePath) {
-    let dir = path.dirname(filePath);
-
-    let packageJsonDirectory = this.walkUntilPackageJsonFound(dir);
-
-    if (packageJsonDirectory === false) {
-      return false;
-    }
-
-    let nodeModules = path.resolve(packageJsonDirectory, 'node_modules/');
-
-    // add their node_modules to the list of resolution paths
-    appModulePath.addPath(nodeModules);
-
-    let result = resolve.sync(partial, {
-      basedir: dir,
-      extensions: [ '.js', '.jsx' ]
-    });
-
-    return result;
-  }
-
-  determineDependentFilesSync(sourceCode, filePath, compilerContext) {
-    let dependencyPartials = detective(sourceCode);
-    let dependencies = [];
-
-    for (let dependencyPartial of dependencyPartials) {
-      
-      if (natives[dependencyPartial]) { 
-        continue;
-      }
-
-      let absolute = this.resolvePartial(dependencyPartial, filePath);
-
-      if (absolute === false) { // obviously invalid?
-        continue;
-      }
-
-      dependencies.push(absolute);
-    }
-
-    return dependencies;
-  }
-
-  compileSync(sourceCode, filePath, compilerContext) {
-    return this.transformCode(sourceCode, filePath);
   }
 
   getCompilerVersion() {
