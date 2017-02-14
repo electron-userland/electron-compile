8dc639d1eb502a7d473372d41693c2263f35e657
Paul Betts <paul@paulbetts.org>
2017-02-14 12:02:13 -0800

    Wire it up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import * as configParser from './config-parser';
 import CompilerHost from './compiler-host';
 import FileChangedCache from './file-change-cache';
 import CompileCache from './compile-cache';
+import {enableLiveReload} from './live-reload';
 
 module.exports = Object.assign({},
   configParser,
+  enableLiveReload,
   { CompilerHost, FileChangedCache, CompileCache }
 );
