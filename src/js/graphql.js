39148b91f046141a7e91166e76aa99a2ebfa7165
Konstantin Nesterov <wasd171@gmail.com>
2017-04-14 20:06:57 +0200

    Added GraphQL compiler

---
+++ src/js/graphql.js
@@ -0,0 +1,30 @@
+import {SimpleCompilerBase} from '../compiler-base';
+
+const inputMimeTypes = ['text/graphql'];
+let gql = null;
+
+export default class GraphQLCompiler extends SimpleCompilerBase {
+  constructor() {
+    super();
+  }
+
+  static getInputMimeTypes() {
+    return inputMimeTypes;
+  }
+
+  compileSync(sourceCode) {
+    gql = gql || require('graphql-tag/loader');
+    // This is a stub, original loader calls webpack-specific this.cacheable()
+    const webpackLoaderStub = {
+      cacheable: () => {}
+    };
+    const loader = gql.bind(webpackLoaderStub);
+
+    const js = loader(sourceCode);
+
+    return {
+      code: js,
+      mimeType: 'application/javascript'
+    };
+  }
+}
