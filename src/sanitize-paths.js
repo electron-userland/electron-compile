15d66ed944dc3a60db7dc4238b87ae817679739b
Paul Betts <paul@paulbetts.org>
2017-04-27 12:18:08 -0700

    Bump up realpath cache

--- src/sanitize-paths.js
+++ src/sanitize-paths.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import LRUCache from 'lru-cache';
 
 const d = require('debug')('electron-compile:sanitize-paths');
-const realpathCache = LRUCache({ max: 32 });
+const realpathCache = LRUCache({ max: 1024 });
 
 function cachedRealpath(p) {
   let ret = realpathCache.get(p);
